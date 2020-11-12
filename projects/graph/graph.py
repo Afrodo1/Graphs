ce1d76aa0dee8e668f3344cda4842834b0178b55
Dominic Bridgette <54685761+Afrodo1@users.noreply.github.com>
2020-11-11 20:47:56 -0800

    finished part1

--- projects/graph/graph.py
+++ projects/graph/graph.py
@@ -13,25 +13,29 @@ class Graph:
         """
         Add a vertex to the graph.
         """
-        pass  # TODO
+        self.vertices.update({vertex_id:set()})
+        
 
     def add_edge(self, v1, v2):
         """
         Add a directed edge to the graph.
         """
+        self.vertices[v1].add(v2)
         pass  # TODO
 
     def get_neighbors(self, vertex_id):
         """
         Get all neighbors (edges) of a vertex.
         """
-        pass  # TODO
+        return self.vertices[vertex_id]
+         # TODO
 
     def bft(self, starting_vertex):
         """
         Print each vertex in breadth-first order
         beginning from starting_vertex.
         """
+        
         pass  # TODO
 
     def dft(self, starting_vertex):
